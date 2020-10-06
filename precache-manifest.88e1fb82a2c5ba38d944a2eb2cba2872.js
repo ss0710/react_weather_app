455c1fb12c91a43f9a13ad10f13573aefe0ff1ec
Sudheer Singh <sudheersid0710@gmail.com>
2020-10-06 21:26:45 +0530

    Updates

---
+++ precache-manifest.88e1fb82a2c5ba38d944a2eb2cba2872.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ebb0c4e84b1e2b06e8d8c212b9e0202",
+    "url": "/ss0710/react_weather_app/index.html"
+  },
+  {
+    "revision": "db4983d02b6356d535cb",
+    "url": "/ss0710/react_weather_app/static/css/main.a3ce7334.chunk.css"
+  },
+  {
+    "revision": "64a3d8a1c419617b797b",
+    "url": "/ss0710/react_weather_app/static/js/2.2acb1ce0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ss0710/react_weather_app/static/js/2.2acb1ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db4983d02b6356d535cb",
+    "url": "/ss0710/react_weather_app/static/js/main.3aa0e247.chunk.js"
+  },
+  {
+    "revision": "31176960edbb607d3fd9",
+    "url": "/ss0710/react_weather_app/static/js/runtime-main.bad435ac.js"
+  },
+  {
+    "revision": "f87c5c2b2e46e79d3194b111f9e494d0",
+    "url": "/ss0710/react_weather_app/static/media/lit-cold.f87c5c2b.PNG"
+  },
+  {
+    "revision": "97e40cc407c3313024c79e8bcdc47539",
+    "url": "/ss0710/react_weather_app/static/media/moderate.97e40cc4.PNG"
+  },
+  {
+    "revision": "d1783ed12f23a4e9bb5e0620aceb0edf",
+    "url": "/ss0710/react_weather_app/static/media/warm.d1783ed1.PNG"
+  },
+  {
+    "revision": "92c304f9d43ccfcf9eece297e740f392",
+    "url": "/ss0710/react_weather_app/static/media/winter.92c304f9.PNG"
+  }
+]);
